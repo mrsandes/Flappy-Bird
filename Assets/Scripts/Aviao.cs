e36e336e27964c8040be5a5462b7bb9d83dd66ee
mrsandes <mtsandes@yahoo.com>
2024-04-10 16:06:53 -0300

    first commit

---
+++ Assets/Scripts/Aviao.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aviao : MonoBehaviour {
+    Rigidbody2D fisica;
+
+    private void Awake() {
+        this.fisica = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            this.Impulsionar();
+        }
+    }
+    
+    private void Impulsionar() {
+        this.fisica.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+    }
+}
